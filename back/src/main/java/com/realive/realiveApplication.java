250ff8853fb1271ff56c7c294e2cad0805df002c
MG <error4489@gmail.com>
2025-05-15 19:05:59 +0900

    finally have got it controlled

--- back/src/main/java/com/realive/realiveApplication.java
+++ back/src/main/java/com/realive/realiveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RealiveApplication {
+public class realiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RealiveApplication.class, args);
+		SpringApplication.run(realiveApplication.class, args);
 	}
 
 }
