c4b14f2da492c791bb83858197cd439b1480cc39
MG <error4489@gmail.com>
2025-05-15 18:45:28 +0900

    4444

---
+++ back/src/test/java/com/realive/realive/RealiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realive.realive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
