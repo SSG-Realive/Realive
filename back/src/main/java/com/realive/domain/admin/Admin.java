7a17cb64b5d40c37196eec95c505b8bff1d7c890
Gudo00 <eveiseve12@gmail.com>
2025-05-16 13:40:50 +0900

    Admin Entity

--- back/src/main/java/com/realive/domain/admin/Admin.java
+++ back/src/main/java/com/realive/domain/admin/Admin.java
@@ -1,4 +1,24 @@
 package com.realive.domain.admin;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "admins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
 }
