a6db3d03a6d7c5270d03988590f70814651b5c8d
정찬호 <yjyk0123@naver.com>
2025-05-16 11:11:09 +0900

    common repo, EmailLookup , seller ->entity

---
+++ back/src/main/java/com/realive/repository/common/EmailLookupRepository.java
@@ -0,0 +1,12 @@
+package com.realive.repository.common;
+
+import java.util.Optional;
+
+public interface EmailLookupRepository<T> {
+
+    Optional<T> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+
+    
+}
