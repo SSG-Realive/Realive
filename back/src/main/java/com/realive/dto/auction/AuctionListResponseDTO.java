e6e74f7a10e327d97231334503413d4a1ec151b7
Gudo00 <eveiseve12@gmail.com>
2025-05-19 14:38:09 +0900

    불면성 명시, fromEntity 추가

--- back/src/main/java/com/realive/dto/auction/AuctionListResponseDTO.java
+++ back/src/main/java/com/realive/dto/auction/AuctionListResponseDTO.java
@@ -1,20 +1,30 @@
 package com.realive.dto.auction;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
 // 경매 목록 조회
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class AuctionListResponseDTO {
 
-    private Integer id;
-    private Integer productId;
-    private Integer startPrice;
-    private Integer currentPrice;
-    private LocalDateTime endTime;
-    private boolean isClosed;
+    private final Integer id;
+    private final Integer productId;
+    private final Integer startPrice;
+    private final Integer currentPrice;
+    private final LocalDateTime endTime;
+    private final boolean closed;
+
+    public static AuctionListResponseDTO fromEntity(com.realive.domain.auction.Auction entity) {
+        return AuctionListResponseDTO.builder()
+                .id(entity.getId())
+                .productId(entity.getProductId())
+                .startPrice(entity.getStartPrice())
+                .currentPrice(entity.getCurrentPrice())
+                .endTime(entity.getEndTime())
+                .closed(entity.isClosed())
+                .build();
+    }
 }
