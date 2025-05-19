74b1090f4291b8993119b8ee2a881c16a4c6a7a4
Gudo00 <eveiseve12@gmail.com>
2025-05-19 14:28:02 +0900

    null 처리

--- back/src/main/java/com/realive/dto/auction/AuctionCreateRequestDTO.java
+++ back/src/main/java/com/realive/dto/auction/AuctionCreateRequestDTO.java
@@ -26,8 +26,14 @@ public class AuctionCreateRequestDTO {
     @Future(message = "종료 시간은 현재 시간 이후여야 합니다.")
     private LocalDateTime endTime;
 
+
     public com.realive.domain.auction.Auction toEntity() {
+        if (productId == null || startPrice == null || endTime == null) {
+            throw new IllegalStateException("필수 필드가 누락되었습니다.");
+        }
+
         LocalDateTime now = LocalDateTime.now();
+
         return com.realive.domain.auction.Auction.builder()
                 .productId(this.productId)
                 .startPrice(this.startPrice)
