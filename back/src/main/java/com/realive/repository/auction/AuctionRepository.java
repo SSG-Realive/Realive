fe1fe5da4e9ed42d65e655a808aeec6385085502
Sungjun Park <psj12357@naver.com>
2025-05-21 10:05:05 +0900

    ã조금씩 레포지토리 만드는 중

--- back/src/main/java/com/realive/repository/auction/AuctionRepository.java
+++ back/src/main/java/com/realive/repository/auction/AuctionRepository.java
@@ -1,4 +1,28 @@
 package com.realive.repository.auction;
 
-public class AuctionRepository {
-}
+import com.realive.domain.auction.Auction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuctionRepository extends JpaRepository<Auction, Integer> {
+
+    List<Auction> findByProductId(Integer productId);
+
+    List<Auction> findByStartTimeAfter(LocalDateTime startTime);
+
+    List<Auction> findByEndTimeBefore(LocalDateTime endTime);
+
+    List<Auction> findByIsClosedFalse();
+
+    List<Auction> findByIsClosedTrue();
+
+    Optional<Auction> findByProductIdAndIsClosedFalse(Integer productId);
+
+    List<Auction> findByCurrentPriceGreaterThanEqual(Integer price);
+
+} //일단 g로 넣었습니다.
