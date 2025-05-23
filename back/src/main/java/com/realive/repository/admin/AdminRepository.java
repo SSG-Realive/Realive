5398441f3612c96c87c0fc6d9d413c2c2237e2a8
Gudo <eveiseve12@gmail.com>
2025-05-23 16:37:54 +0900

    관리자 Repo 부분 수정

--- back/src/main/java/com/realive/repository/admin/AdminRepository.java
+++ back/src/main/java/com/realive/repository/admin/AdminRepository.java
@@ -1,12 +1,13 @@
 package com.realive.repository.admin;
 
+import com.realive.domain.admin.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.realive.domain.admin.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
     Optional<Admin> findByEmail(String email);
 }
