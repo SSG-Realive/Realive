a453b305054c03ba3625cf4dbd7b5dd4b0d245d1
정찬호 <yjyk0123@naver.com>
2025-05-29 15:33:47 +0900

    0529/1533

--- back/src/main/java/com/realive/repository/order/SellerOrderDeliveryRepository.java
+++ back/src/main/java/com/realive/repository/order/SellerOrderDeliveryRepository.java
@@ -8,15 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface SellerOrderDeliveryRepository extends JpaRepository<OrderDelivery, Long> {
+public interface SellerOrderDeliveryRepository extends JpaRepository<OrderDelivery, Long> , SellerOrderDeliveryRepositoryCustom {
 
-    // ✅ 주문 ID를 기준으로 배송 정보를 조회
-    Optional<OrderDelivery> findByOrderId(Long orderId);
-
-    // ✅ 판매자 기준 배송 목록 (이미 있는 메서드)
-    @Query("SELECT d FROM OrderDelivery d " +
-            "JOIN d.order o " +
-            "JOIN o.product p " +
-            "WHERE p.seller.id = :sellerId")
-    List<OrderDelivery> findAllBySellerId(@Param("sellerId") Long sellerId);
+   
 }
