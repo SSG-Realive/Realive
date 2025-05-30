9fbceb3bf852a26019d86e5c841ed368b1b3a478
정찬호 <yjyk0123@naver.com>
2025-05-30 17:33:21 +0900

    git fetch origi

--- back/src/main/java/com/realive/config/SellerSecurityConfig.java
+++ back/src/main/java/com/realive/config/SellerSecurityConfig.java
@@ -5,8 +5,8 @@
     import lombok.extern.slf4j.Slf4j;
 
     import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+    import org.springframework.core.annotation.Order;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
     import org.springframework.security.config.annotation.web.builders.HttpSecurity;
     import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
     import org.springframework.security.config.http.SessionCreationPolicy;
