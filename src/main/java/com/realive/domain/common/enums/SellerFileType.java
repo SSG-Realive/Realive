a3c9aeeed41c499711466444b8a1ee74ed99eb61
정찬호 <yjyk0123@naver.com>
2025-06-10 15:17:50 +0900

    Initial commit

---
+++ src/main/java/com/realive/domain/common/enums/SellerFileType.java
@@ -0,0 +1,6 @@
+package com.realive.domain.common.enums;
+
+public enum SellerFileType {
+    사업자등록증,
+        통장사본
+}
---
+++ src/main/java/com/realive/service/cart/view/CartViewService.java
@@ -0,0 +1,7 @@
+package com.realive.service.cart.view;
+
+import com.realive.dto.cart.CartListResponseDTO;
+
+public interface CartViewService {
+    CartListResponseDTO getCart(Long customerId);
+}
