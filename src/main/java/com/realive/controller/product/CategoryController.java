e73211631e676fdcc5b6546bd697c3cbf62617d4
정찬호 <yjyk0123@naver.com>
2025-06-11 13:24:06 +0900

    컨트롤러 수정 0611/1324

--- src/main/java/com/realive/controller/product/CategoryController.java
+++ src/main/java/com/realive/controller/product/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/categories")
+@RequestMapping("/api/seller/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
